fd4ae8c8c623a34fbf8855108ba03f4eef129a8b
s338411 <musterman@mail.com>
2022-06-12 17:02:29 +0200

    Init Spring

---
+++ src/main/java/com/example/chuckjok/ChuckjokApplication.java
@@ -0,0 +1,13 @@
+package com.example.chuckjok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckjokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckjokApplication.class, args);
+	}
+
+}
